a8a1e77bc8c6a10652888760f48f189dccb6dc11
odlanyers <rey.san.rey@gmail.com>
2024-02-28 18:43:07 -0600

    Adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\LoginController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Renderiza la página de login
+Route::view('/', 'login')->name('login')->middleware('guest');
+
+// Realiza la petición post para el login del usuario
+Route::post('login', [LoginController::class, 'login'])->name('signin');
+// Petición delete para eliminar la sesión del usuario
+Route::delete('logout', [LoginController::class, 'logout']);
+
+// Rutas protegidas por autenticación de usuario
+Route::middleware('auth')->group(function() {
+    // Renderiza la página para subir archivos
+    Route::view('upload-file', 'upload_file');
+    // Petición post para subir archivos
+    Route::post('upload-file', [ExpenseController::class, 'importExcel'])->name('upload-file');
 });
