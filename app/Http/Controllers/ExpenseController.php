e496492a8fcffbc451ea6726328706f5a54587c4
odlanyers <rey.san.rey@gmail.com>
2024-02-28 18:34:32 -0600

    Adding method for processing file

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Imports\ExpensesImport;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ExpenseController extends Controller
 {
-    //
+    public function importExcel(Request $request)
+    {
+        // Validación del archivo subido y su formato
+        $request->validate([
+            'file' => 'required|mimes:xlsx'
+        ]);
+
+        // Importación de archivo utilizando la clase ExpensesImport
+        Excel::import(new ExpensesImport, $request->file('file'));
+
+        // Respuesta a la petición
+        return redirect('/')->with('success', 'Archivo importado exitosamente');
+    }
 }
